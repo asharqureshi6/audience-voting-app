f5d72ecb81cad7666cecb710ebc1937fcb504ae9
asharqureshi6 <92637237+asharqureshi6@users.noreply.github.com>
2025-10-31 15:17:38 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Host from "./Host";
+import Vote from "./Vote";
+import Display from "./Display";
 
-function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+export default function App() {
+  return (
+    <BrowserRouter>
+      <nav style={{padding:10}}>
+        <Link to="/host" style={{marginRight:10}}>Host</Link>
+        <Link to="/vote" style={{marginRight:10}}>Vote</Link>
+        <Link to="/display">Display</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Host />} />
+        <Route path="/host" element={<Host />} />
+        <Route path="/vote" element={<Vote />} />
+        <Route path="/display" element={<Display />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
-export default App;
